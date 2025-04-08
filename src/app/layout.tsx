cccf8a4d96c08659a0a8e3870665d77430e1625d
Hardikv25 <virashardik2003@gmail.com>
2025-04-08 13:52:30 +0530

    meta added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,10 @@ import Footer from "./components/Footer";
 import SendMessage from "./components/sendMessage";
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "My Nextjs portfolio website",
+  title: "Hardik Viras | Full-Stack Developer",
+  description: "Portfolio site of Hardik Viras, a full-stack developer specializing in React, Next.js, Laravel, and MySQL.",
+  keywords: ["Next.js portfolio", "Full-Stack Developer", "React Developer", "Laravel", "MCA student", "Hardik Viras", "Hardik Viras Portfolio"],
+  authors: [{ name: "Hardik Viras" }],
 };
 
 export default function RootLayout({
@@ -16,11 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:title" content="Hardik Viras | Portfolio" />
+        <meta property="og:description" content="A showcase of my projects, blogs, and technical skills." />
+        <meta property="og:image" content="https://hardik-viras-portfolio.vercel.app/myimg2.jpg" />
+        <meta property="og:url" content="https://hardik-viras-portfolio.vercel.app/" />
+      </head>
       <body className="p-4 sm:p-8">
-        <Header/>
+        <Header />
         {children}
-        <SendMessage/>
-        <Footer/>
+        <SendMessage />
+        <Footer />
       </body>
     </html>
   );
